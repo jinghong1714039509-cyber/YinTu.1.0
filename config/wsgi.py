ce886bdb70800969ddd0a36fbf3e0884a81efe39
jinghong1714039509-cyber <jinghong1714039509@gmail.com>
2025-11-28 16:59:19 +0800

    11/28/17:00

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,1 +1,9 @@
 # config.wsgi
+import os
+from django.core.wsgi import get_wsgi_application
+
+# 告诉 Django 使用哪个配置文件
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
+# 创建应用实例 (这就是报错说找不到的 application)
+application = get_wsgi_application()
