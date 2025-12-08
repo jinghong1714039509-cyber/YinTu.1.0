4b1511cb0d05d737f45075fa20a394b478546e79
jinghong1714039509-cyber <jinghong1714039509@gmail.com>
2025-12-08 16:21:17 +0800

    10

--- apps/hospital/urls.py
+++ apps/hospital/urls.py
@@ -1,15 +1,16 @@
 from django.urls import path
 from . import views
 
+# 命名空间，对应模板中的 'hospital:xxx'
 app_name = 'hospital'
 
 urlpatterns = [
-    # ✅ 新增这一行：处理 /hospital/ 根路径，默认显示列表页
-    path('', views.index, name='home'),
+    # 列表页
+    # 对应模板: {% url 'hospital:index' %}
+    path('', views.index, name='index'),
 
-    # A端新建任务
+    # 新建任务页
+    # 对应模板: {% url 'hospital:add' %}
+    # 原报错行: views.create_dataset_task -> 改为 views.add_task
     path('add/', views.add_task, name='add'),
-    
-    # A端任务列表
-    path('index/', views.index, name='index'), 
 ]
